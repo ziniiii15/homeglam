5a07fc9f4ac718cab33cec91b7237f2a02aba739
ziniiii15 <arnado.jennymae@llcc.edu.ph>
2026-03-14 11:53:54 +0800

    Fix image upload path and add transaction modals

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
